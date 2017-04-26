3691325f367d0c3e3ee059b5808b6bca5f300b73
Tom Sanders <tom.sanders@inqwell.com>
2017-04-26 12:48:00 +0100

    Fix description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject typeops "0.1.0-alpha"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Alternative type outcomes for arithmetic"
+  :url "https://github.com/inqwell/typeops"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
